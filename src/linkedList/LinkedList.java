cb21f4e38b693feb6fd83e952e3cd449464cb029
Krishnkant Nagar <krishnkant13@gmail.com>
2024-05-25 11:10:10 +0530

    first commit in linklist

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,7 +1,9 @@
 package linkedList;
 
 public class LinkedList {
-	
+
+	Node head;
+
 	static class Node{
 		int data;
 		Node next;
@@ -11,14 +13,49 @@ public class LinkedList {
 			this.next=null;
 		}
 	}
-	
-	private Node head;
-	
-	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public boolean add(int data){
+		Node node=new Node(data);
+		if(head==null) {
+			head = node;
+			return true;
+		}
+		Node current = head;
+		while(current.next!=null)
+			current= current.next;
+		current.next=node;
+        return false;
+    }
 
+	public  void remove(int data){
+		if (head==null)
+			return;
+		if (head.data==data)
+			head=head.next;
+		Node curr =head;
+		while (curr.next!=null){
+			if (curr.next.data==data)
+				curr.next=curr.next.next;
+			curr=curr.next;
+		}
 	}
 
+	public  void printlist(){
+		Node curr=head;
+		while (curr.next!=null){
+			System.out.print(curr.data+" ");
+			curr=curr.next;
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		LinkedList list= new LinkedList();
+		list.add(10);
+		list.add(20);
+		list.printlist();
+		list.remove(10);
+		list.printlist();
+
+	}
 }
