6b3a5d6cc961c107f2d03edbdae0580baf11f181
Krishnkant Nagar <krishnkant13@gmail.com>
2024-06-06 18:40:02 +0530

    implement reverse method in LinkedList

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -42,11 +42,26 @@ public class LinkedList {
 
 	public  void printlist(){
 		Node curr=head;
-		while (curr.next!=null){
+		while (curr!=null){
 			System.out.print(curr.data+" ");
 			curr=curr.next;
 		}
 		System.out.println();
+	}
+
+	Node reverse()
+	{	Node node=head;
+		Node prev = null;
+		Node current = node;
+		Node next = null;
+		while (current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		node = prev;
+		return node;
 	}
 
 	public static void main(String[] args) {
